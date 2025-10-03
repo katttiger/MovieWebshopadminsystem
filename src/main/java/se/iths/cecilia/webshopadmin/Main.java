a292a5d80d0a53778ee63407410333b5f616eaf4
katttiger <katttiger@gmail.com>
2025-10-03 15:47:27 +0200

    Final polish and removal of redundant code.

--- src/main/java/se/iths/cecilia/webshopadmin/Main.java
+++ src/main/java/se/iths/cecilia/webshopadmin/Main.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    private final static Scanner sc = new Scanner(System.in);
+    private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
 
